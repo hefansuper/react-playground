24546510e6af57c614b69c9ff811db90b808c402
stephenHe <hf597833184@163.com>
2025-03-31 21:52:49 +0800

    feat: 1:解决编辑器提示错误的问题,2:增加部分的操作按键,3:设置部分的样式默认值

--- src/ReactPlayground/components/CodeEditor/Editor.tsx
+++ src/ReactPlayground/components/CodeEditor/Editor.tsx
@@ -2,24 +2,57 @@
  * @Author: stephenHe
  * @Date: 2025-03-31 17:53:28
  * @LastEditors: stephenHe
- * @LastEditTime: 2025-03-31 17:57:42
+ * @LastEditTime: 2025-03-31 21:51:03
  * @Description: 编辑器部分
  * @FilePath: /react playground/src/ReactPlayground/components/CodeEditor/Editor.tsx
  */
-import MonacoEditor from "@monaco-editor/react";
+import MonacoEditor, { OnMount } from "@monaco-editor/react";
 
 export default function Editor() {
   const code = `export default function App() {
     return <div>xxx</div>
-}
+  }
     `;
+
+  // 设置编辑器的默认配置
+
+  const handleEditorMount: OnMount = (editor, monaco) => {
+    // 1：解决编辑器报错的问题
+    monaco.languages.typescript.typescriptDefaults.setCompilerOptions({
+      // jsx设置为保留 输入 <div> 输出 <div>，保留原样
+      jsx: monaco.languages.typescript.JsxEmit.Preserve,
+      // 设置 esModuleInterop 会在编译的时候自动加上 default 属性。可以使用 import React from 'react'，而不是 import * as React from 'react'
+      esModuleInterop: true,
+    });
+
+    // 2：设置编辑器的快捷键
+    // cmd + J 格式化代码
+    editor.addCommand(monaco.KeyMod.CtrlCmd | monaco.KeyCode.KeyJ, () => {
+      editor.getAction("editor.action.formatDocument")?.run();
+    });
+  };
 
   return (
     <MonacoEditor
-      height="100%"
+      height={"100%"}
       path={"guang.tsx"}
       language={"typescript"}
+      onMount={handleEditorMount}
       value={code}
+      options={{
+        fontSize: 14,
+        // 是到了最后一行之后依然可以滚动一屏，关闭后就不会了
+        scrollBeyondLastLine: false,
+        // 缩略图
+        minimap: {
+          enabled: false,
+        },
+        // 滚动条
+        scrollbar: {
+          verticalScrollbarSize: 6,
+          horizontalScrollbarSize: 6,
+        },
+      }}
     />
   );
 }
