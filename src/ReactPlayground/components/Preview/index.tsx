67404ceac88282edf0b6eaa4cf07d0457a4aca95
stephenHe <hf597833184@163.com>
2025-04-03 11:10:50 +0800

    feat: 增加iframe的预览

--- src/ReactPlayground/components/Preview/index.tsx
+++ src/ReactPlayground/components/Preview/index.tsx
@@ -1,11 +1,36 @@
-import { useContext, useEffect, useState } from "react";
+import { useCallback, useContext, useEffect, useState } from "react";
+
 import { PlaygroundContext } from "../../PlaygroundContext";
-import Editor from "../CodeEditor/Editor";
 import { compile } from "./compiler";
+import iframeRaw from "./iframe.html?raw";
+import { IMPORT_MAP_FILE_NAME } from "../../files";
 
 export default function Preview() {
   const { files } = useContext(PlaygroundContext);
   const [compiledCode, setCompiledCode] = useState("");
+
+  const getIframeUrl = useCallback(() => {
+    // 将编译后的代码和 importmap 插入到 iframe 中
+    const res = iframeRaw
+      .replace(
+        '<script type="importmap"></script>',
+        `<script type="importmap">${files[IMPORT_MAP_FILE_NAME].value}</script>`
+      )
+      .replace(
+        '<script type="module" id="appSrc"></script>',
+        `<script type="module" id="appSrc">${compiledCode}</script>`
+      );
+
+    // 给到iframe的时候也是生成的html的blob url
+    return URL.createObjectURL(new Blob([res], { type: "text/html" }));
+  }, [compiledCode, files]);
+
+  const [iframeUrl, setIframeUrl] = useState(getIframeUrl());
+
+  useEffect(() => {
+    // 每次左边编辑器内容变化时，重新生成iframe的url
+    setIframeUrl(getIframeUrl());
+  }, [compiledCode, getIframeUrl]);
 
   useEffect(() => {
     const res = compile(files);
@@ -14,11 +39,13 @@ export default function Preview() {
 
   return (
     <div style={{ height: "100%" }}>
-      <Editor
-        file={{
-          name: "dist.js",
-          value: compiledCode,
-          language: "javascript",
+      <iframe
+        src={iframeUrl}
+        style={{
+          width: "100%",
+          height: "100%",
+          padding: 0,
+          border: "none",
         }}
       />
     </div>
